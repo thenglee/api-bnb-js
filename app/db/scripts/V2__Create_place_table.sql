870df9c91f67803b1b624ab2e4c23b5b2fbf8d6b
thenglee <thengleecoding@gmail.com>
2021-08-27 18:27:19 +0800

    Modify SQL scripts to add more columns to users and places tables

--- app/db/scripts/V2__Create_place_table.sql
+++ app/db/scripts/V2__Create_place_table.sql
@@ -4,11 +4,11 @@ CREATE TABLE places (
   country VARCHAR(50) NOT NULL,
   state VARCHAR(40) NOT NULL,
   city VARCHAR(40) NOT NULL,
-  postal_code VARCHAR(30),
+  postal_code VARCHAR(30) NOT NULL,
   name VARCHAR(80) NOT NULL,
   description VARCHAR(400),
-  price NUMERIC(7,2),
-  available BOOLEAN NOT NULL DEFAULT TRUE,
+  price NUMERIC(7,2) NOT NULL,
+  available BOOLEAN DEFAULT FALSE NOT NULL ,
   PRIMARY KEY(id),
   CONSTRAINT fk_user
     FOREIGN KEY(user_id)
